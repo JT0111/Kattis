8fa9b05aa9d854915d9c916032321b9751be2f33
JT0111 <jt1g19@soton.ac.uk>
2020-03-28 17:26:20 +0000

    Beggining of 2nd part but tbh I'm lost

--- ADifferentListGame.cpp
+++ ADifferentListGame.cpp
@@ -45,9 +45,20 @@ int main()
         }
         k+=6;
     }
+
+    //just a "helper" output - will be removed when code is complete
     for(int i=0; i<V.size(); i++){
         cout<<V[i]<<" ";
     }
+    cout<<endl;
+
+
+    //I have no idea if that's gonna work but the idea is:
+    //->make all 1 element distinct subsets
+    //then follow to 2, 3, 4 until there's just one value which will be divided as long as possible
+
+    int il=V.size(); //number of 1-element subsets
+    int end=V.size()-1;
 
     return 0;
 }
